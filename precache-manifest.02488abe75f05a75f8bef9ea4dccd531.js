d90cbf5c49fea53fb93f75e075b36dd0512083ff
WomackCodes <chris@womack.codes>
2020-10-16 14:29:53 -0700

    Updates

---
+++ precache-manifest.02488abe75f05a75f8bef9ea4dccd531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4c4ae2d51a630c8cb61e004bccdb0d",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "2d0f394f158d59157707",
+    "url": "/project-4-assessment/static/css/main.bd81d22f.chunk.css"
+  },
+  {
+    "revision": "608118624ae7ee197d4b",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-4-assessment/static/js/2.e208ecfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0f394f158d59157707",
+    "url": "/project-4-assessment/static/js/main.cc967ed3.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
